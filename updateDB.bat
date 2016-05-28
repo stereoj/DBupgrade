004c539565cb17b221628b0ef82d2d63e01cb4ea
stereoj <stereoj@gmail.com>
2016-05-28 21:12:46 +0300

    Update updateDB.bat

--- updateDB.bat
+++ updateDB.bat
@@ -1,8 +1,10 @@
 @echo off
 
-set /a currentVersion=47
+set /p currentVersion=<version.txt
 
-CD C:\UPDATES\SCRIPTS
+CD C:\TEMP\SCRIPTS
+ECHO %currentVersion%
 
-FOR %%I IN (045 046 047 048 049 050 051 052) DO IF "currentVersion" LSS "%%I" Update.bat %%I
+DIR
 
+REM FOR %%I IN (045 046 047 048 049 050 051 052) DO IF "currentVersion" LSS "%%I" Update.bat %%I
